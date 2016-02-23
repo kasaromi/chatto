8ea98207837b6cbc86d9d4a3da8a057efc9e8131
shouston3 <shouston3@me.com>
2016-02-23 16:57:36 +0000

    setting up redis and server

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 var http = require('http');
 var port = 8000;
 var fs = require('fs');
+var redisFunctions = require('./redis.js');
 
 function handler(req, res){
     var url = req.url;
@@ -9,11 +10,29 @@ function handler(req, res){
             if (error) {
                 console.log(error);
             } else {
-                console.log("------");
                 res.writeHead(200, {'Content-Type': 'text/html'});
                 res.end(index);
             }
         });
+    }
+    else if(url.indexOf('.')>-1){
+        var file = url;
+        var ext = url.split('.')[1];
+        fs.readFile(__dirname + '/../' + url, function(error, file) {
+            if (error) {
+                console.log(error);
+            } else {
+                res.writeHead(200, {'Content-Type': 'text/' + ext});
+                res.end(file);
+            }
+        });
+    }
+    else if( url.indexOf(':')>-1){
+        var total = url.split('/')[1].split(':');
+        var room = total[0];
+        var user = total[1];
+        var message = total[2];
+        redisFunctions.addToDB(room, user, message);
     }
 }
 
--- test/server.test.js
+++ test/server.test.js
@@ -1,7 +1,7 @@
-var http = require('http');
 var tape = require('tape');
 var server = require('../server/server.js');
 var shot = require('shot');
+var fs = require('fs');
 
 tape('Does server respond successfully?', function(t) {
     shot.inject(server.handler, {method: 'GET', url:'/'}, function(res) {
@@ -14,6 +14,15 @@ tape('Does the server read the index.html file', function(t){
     shot.inject(server.handler, {method: 'Get', url:'http://localhost:8000'}, function(res){
         t.notEqual(res.payload.indexOf("<!DOCTYPE html>"), -1, 'server returns html page');
         t.end();
+    });
+});
+
+tape('Other file types can be returned successfully', function(t) {
+    shot.inject(server.handler, {method: 'Get', url: 'http://localhost:8000/style.css'}, function(res) {
+        fs.readFile(__dirname + '/../style.css', function(err, cssText) {
+            t.equal(res.payload, cssText.toString(), 'server returns css page');
+            t.end();
+        });
     });
 });
 
