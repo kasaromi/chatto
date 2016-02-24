2ea2839affceb1e2db82470a193c069b285ccb64
Mireia <mireia@mireiasangalo.com>
2016-02-24 15:34:25 +0000

    linked handler and redis

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ document.getElementById('send').addEventListener('click', function(k){
     var msg = document.getElementById('mess').value;
     k.preventDefault();
     socket.emit('chat message', username + ': ' + msg);
+    sendMessage(username, msg);
     document.getElementById('mess').value='';
 });
 
@@ -14,3 +15,26 @@ socket.addEventListener('chat message', function(msg){
     msgOnScreen.appendChild(msgText);
     document.getElementById('messageContainer').appendChild(msgOnScreen);
 });
+
+
+var request = new XMLHttpRequest();
+
+function sendMessage(name, message){
+    request.open('post', '/msg' + name + ':' + message);
+    request.send();
+}
+
+function loadData(){
+    request.onreadystatechange = function(){
+        if(request.readyState === 4 && request.status === 200){
+            var data = request.responseText;
+            var node = document.createTextNode(data);
+            var other = document.createElement('div');
+            other.appendChild(node);
+            document.getElementById('messageContainer').appendChild(other);
+        }
+    };
+    request.open('get', '/display');
+    request.send();
+}
+loadData();
