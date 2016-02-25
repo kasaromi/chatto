17ab10fd0d478c0011dc2e0000a2f4c434a8366d
shouston3 <shouston3@me.com>
2016-02-25 19:22:36 +0000

    sorted out function

--- index.js
+++ index.js
@@ -8,9 +8,8 @@ var socket = io();
 // number is the message number, type can be: time, name and message and input is the inputted object
 function format(number, type, input){
     if(type === 'time'){
-        var months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
-        date = new Date(Object.keys(input)[(number - 1) * 2].split('name')[1] * 1000);
-        return [date.getDate(), months[date.getMonth()], date.getHours(), date.getMinutes()];
+        var dateMili = Object.keys(input)[(number - 1) * 2].split('name')[1] * 10;
+        return dateMili;
     }
     else if(type === 'name'){
         x = Object.keys(input).map(function (key){
@@ -24,6 +23,24 @@ function format(number, type, input){
         });
         return x[(number -1) * 2 + 1];
     }
+}
+
+//data = {"name145641721327":"jk","message145641721327":"hi"};
+function print(data){
+    var months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
+    var formattedDate = format(1, 'time', JSON.parse(data));
+    var date = new Date(formattedDate);
+    var day = date.getDate().toString();
+    var month = months[date.getMonth()].toString();
+    var hours = date.getHours().toString();
+    var mins = date.getMinutes().toString();
+    var concatedDate = day + " " + month + " - " + hours + ":" + mins;
+    console.log(concatedDate);
+    var string = document.createTextNode(concatedDate);
+    var node = string;
+    var other = document.createElement('div');
+    other.appendChild(node);
+    document.getElementById('messageContainer').appendChild(other);
 }
 
 document.getElementById('send').addEventListener('click', function(k){
@@ -61,10 +78,11 @@ function loadData(){
     request.onreadystatechange = function(){
         if(request.readyState === 4 && request.status === 200){
             var data = request.responseText;
-            var node = document.createTextNode(data);
-            var other = document.createElement('div');
-            other.appendChild(node);
-            document.getElementById('messageContainer').appendChild(other);
+            print(data);
+            // var node = document.createTextNode(data);
+            // var other = document.createElement('div');
+            // other.appendChild(node);
+            // document.getElementById('messageContainer').appendChild(other);
         }
     };
     request.open('get', '/display');
