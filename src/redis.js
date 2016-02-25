b94bee66fecdf746702c04c8031581331ee2ba74
shouston3 <shouston3@me.com>
2016-02-25 09:36:48 +0000

    debuggin

--- src/redis.js
+++ src/redis.js
@@ -2,7 +2,7 @@ var redis = require('redis');
 var client = redis.createClient();
 
 function setData(cli, x, y){
-    client.hmset(cli, 'name' /*+ Date.now()*/, x, 'message' /*+ Date.now()*/, y);
+    client.hmset(cli, 'name' + Date.now(), x, 'message' + Date.now(), y);
 }
 
 function getData(cli, callback){
