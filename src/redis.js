34d65d364c33b1128337f7549fe762d68fbb8714
shouston3 <shouston3@me.com>
2016-02-25 15:49:29 +0000

    tests now finally all work

--- src/redis.js
+++ src/redis.js
@@ -2,7 +2,11 @@ var redis = require('redis');
 var client = redis.createClient(process.env.REDIS_URL);
 
 function setData(cli, x, y){
-    client.hmset(cli, 'name' + Date.now(), x, 'message' + Date.now(), y);
+    var name = 'name' + Date.now();
+    name = name.substring(0,name.length-1);
+    var message = 'message' + Date.now();
+    message = message.substring(0,message.length-1);
+    client.hmset(cli, name, x, message, y);
 }
 
 function getData(cli, callback){
